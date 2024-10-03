589045d320114225776facbc3713f8f661581fde
Max Greenwald <maxsupergreenwald@gmail.com>
2024-10-03 18:20:38 -0400

    Updates

---
+++ precache-manifest.bed7134760cef07d08b70ffd99e99710.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588edb8ffed5142aaf01b96e24242365",
+    "url": "/spacejunk_2/index.html"
+  },
+  {
+    "revision": "42eafd313aede1d9a2e9",
+    "url": "/spacejunk_2/static/css/main.d2ac7f06.chunk.css"
+  },
+  {
+    "revision": "810a8f0b043b0864bdc4",
+    "url": "/spacejunk_2/static/js/2.b6ce5ce6.chunk.js"
+  },
+  {
+    "revision": "a4fb08d5dc8d3fa9fba39c69dd7cca4b",
+    "url": "/spacejunk_2/static/js/2.b6ce5ce6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42eafd313aede1d9a2e9",
+    "url": "/spacejunk_2/static/js/main.13724663.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/spacejunk_2/static/js/main.13724663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fc464190bbb7e567241",
+    "url": "/spacejunk_2/static/js/runtime-main.f53d4f8d.js"
+  },
+  {
+    "revision": "cd76a9db8f032315b7b70836d613b81f",
+    "url": "/spacejunk_2/static/media/background_level1.cd76a9db.jpg"
+  },
+  {
+    "revision": "b35bd0767320b89183cd0d126a6cc13d",
+    "url": "/spacejunk_2/static/media/background_level6.b35bd076.jpg"
+  }
+]);
